e1c0a845d35e102eedfb56c73e769df99075ed42
wenbin <2251039220@qq.com>
2020-10-29 21:31:18 +0800

    猜数字游戏修改

--- guess_number.py
+++ guess_number.py
@@ -19,16 +19,20 @@ if __name__ == "__main__":
     n = 1
     # 循环判断直到正确
     while True:
-        num_guess = int(input('请猜想并输入一个0-100之间的数字：\n'))
-        if 1 <= num_guess <= 100:
-            m = guess_num(num_guess, num_real)
-            if m == False:
-                break
-            # n += 1
+        try:
+            num_guess = int(input('请猜想并输入一个0-100之间的数字：\n'))
+            if 1 <= num_guess <= 100:
+                m = guess_num(num_guess, num_real)
+                if m == False:
+                    break
+                # n += 1
 
-        else:
-            print('重新开始游戏，请输入一个100以内的数字。\n')
-            
+            else:
+                print('继续游戏，请输入一个100以内的数字。\n')
+
+        except:
+            print('继续游戏，请输入0-100之间的数字。\n')
+
         n += 1
 
     print('这次游戏，你一共猜测了%d次。' % n)
